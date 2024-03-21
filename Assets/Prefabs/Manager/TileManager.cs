e63994847d13e0925fc2424d4d6e0ff85e94e9d6
TerrenceC1211 <70148874+TerrenceC1211@users.noreply.github.com>
2024-03-21 13:39:24 +0800

    a

---
+++ Assets/Prefabs/Manager/TileManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class TileManager : MonoBehaviour
+{
+    public TileBase tileA;
+    public TileBase tileB;
+    public BoundsInt area; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
